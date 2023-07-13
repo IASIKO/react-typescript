b04a70e29f548960f07c41abc3e56fcf9d523111
IASIKO <iaseshviligi@gmail.com>
2023-07-13 16:19:27 +0400

    add

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Todo from "../model/todo";
+import TodoItem from "./TodoItem";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
-      {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+      {props.items.map((item: Todo) => (
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
