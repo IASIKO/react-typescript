eecf3b1ff20b10b405311719c94cd15eb55a6c22
IASIKO <iaseshviligi@gmail.com>
2023-07-14 16:22:51 +0400

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./model/todo";
 
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
+  // const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
 
-  const addTodoHandler = (todoText: string) => {};
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodo) => {
+      return prevTodo.concat(newTodo);
+    });
+  };
 
   return (
     <div>
