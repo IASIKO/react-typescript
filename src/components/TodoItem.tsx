6e8377c96aef15b5cbffa74df04bd35804b68316
IASIKO <iaseshviligi@gmail.com>
2023-07-14 17:47:42 +0400

    add

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,4 @@
-import classes from './TodoItem.module.css'
+import classes from "./TodoItem.module.css";
 
 const TodoItem: React.FC<{ text: string }> = (props) => {
   return <li className={classes.item}>{props.text}</li>;
